c04a0f33f6db2998b589baf383671eb9d80e845b
ryanp <ryanpanimation@gmail.com>
2020-07-10 00:01:33 -0500

    all GET routing done

--- data/apiRouter.js
+++ data/apiRouter.js
@@ -10,10 +10,34 @@ const Data = require("./db");
 router.get("/", async (req, res) => {
   try {
     const data = await Data.find();
-    data ? res.status(200).json(data) : res.status(404);
+    data ? res.status(200).json(data) : res.status(404).json({ message: "No posts found." });
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    res.status(500).json({ error: "The posts information could not be retrieved." });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await Data.findById(req.params.id);
+    data
+      ? res.status(200).json(data)
+      : res.status(404).json({ message: "The post with the specified ID does not exist." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The posts information could not be retrieved." });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const data = await Data.findPostComments(req.params.id);
+    data
+      ? res.status(200).json(data)
+      : res.status(404).json({ message: "The post with the specified ID does not exist." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The comments information could not be retrieved." });
   }
 });
 
