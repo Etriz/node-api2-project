9255d4401f7c1ab497ea7d10406e3e83619939ab
ryanp <ryanpanimation@gmail.com>
2020-07-12 23:10:15 -0500

    all routes working

--- data/apiRouter.js
+++ data/apiRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Data = require("./db");
+const { findById } = require("./db");
 
 router.get("/", async (req, res) => {
   try {
@@ -15,8 +16,9 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const data = await Data.findById(req.params.id);
-    data
+    const { id } = req.params;
+    const data = await Data.findById(id);
+    data.length !== 0
       ? res.status(200).json(data)
       : res.status(404).json({ message: "The post with the specified ID does not exist." });
   } catch (error) {
@@ -27,8 +29,9 @@ router.get("/:id", async (req, res) => {
 
 router.get("/:id/comments", async (req, res) => {
   try {
-    const data = await Data.findPostComments(req.params.id);
-    data
+    const { id } = req.params;
+    const data = await Data.findPostComments(id);
+    data.length !== 0
       ? res.status(200).json(data)
       : res.status(404).json({ message: "The post with the specified ID does not exist." });
   } catch (error) {
@@ -38,9 +41,9 @@ router.get("/:id/comments", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const data = await Data.insert(req.body);
   try {
     if (req.body.title && req.body.contents) {
+      const data = await Data.insert(req.body);
       if (data) {
         res.status(201).json(req.body);
       }
@@ -53,26 +56,56 @@ router.post("/", async (req, res) => {
   }
 });
 
-// router.post("/:id/comments", async (req, res) => {
-//   try {
-//     const data = await Data.insertComment(req.body);
-//     if (data) {
-//       res.status(201).json(req.body);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "There was an error while saving the comment to the database" });
-//   }
-// });
+router.post("/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
+    const postID = await findById(id);
+    if (postID.length > 0 && text) {
+      const data = await Data.insertComment({ text: text, post_id: id });
+      data.length !== 0 ? res.status(201).json({ text: text }) : null;
+    } else if (postID.length === 0) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    } else if (!text) {
+      res.status(400).json({ errorMessage: "Please provide text for the comment." });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "There was an error while saving the comment to the database" });
+  }
+});
 
 router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const data = await Data.remove(id);
-    if (data) res.status(200).json({ message: `Post ${id} Deleted` });
+    if (data) {
+      res.status(200).json({ message: `Post ${id} has been deleted` });
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "The post could not be removed" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const idExists = await findById(id);
+    const post = req.body;
+    if (idExists.length > 0 && post.title && post.contents) {
+      const data = await Data.update(id, post);
+      res.status(200).json(post);
+    } else if (idExists.length === 0) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    } else if (!post.title || !post.contents) {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The post information could not be modified." });
   }
 });
 
